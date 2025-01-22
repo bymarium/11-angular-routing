de8fdeea9801259bf59a9a18e16b15bae98e1ba2
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2025-01-22 09:54:41 -0500

    heohjdoafmkl;alsdjnfggkjdbhsgfhasjgjfhgsd

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,19 +1,48 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  constructor(private activatedRoute: ActivatedRoute) {
+  private formBuilder = inject(FormBuilder);
 
+  public loginForm = this.formBuilder.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', [Validators.minLength(8), Validators.required]],
+    address: this.formBuilder.group({
+      street: [''],
+      number: [''],
+      zipCode: ['']
+    }),
+    tags: this.formBuilder.array([this.formBuilder.control('')])
+  })
+
+  get tags(): FormArray {
+    return this.loginForm.get('tags') as FormArray;
   }
 
-  handle(): void {
-    this.activatedRoute.params.subscribe(console.log)
-    this.activatedRoute.data.subscribe(console.log)
+  submit(): void {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+
+    console.log(this.loginForm);
+    
   }
+
+  addTag(): void {
+    this.tags.push(this.formBuilder.control(''))
+  }
+
+  // removeTag(index: number): void {
+  //   const controls = this.tags.controls.filter((control) => control !== value)
+  //   this.tags.clear();
+  //   controls.forEach((control) => this.tags.push(control));
+
+  //   console.log(this.tags.controls);
+  // }
 }
