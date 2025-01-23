7e3dcd8137e6250917bb24cb894c45756d26a7db
Bymarium <bymarium@gmail.com>
2025-01-23 10:42:52 -0500

    create footer component and adjust styles

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AsideComponent } from "../aside/aside.component";
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [AsideComponent, RouterOutlet],
+  imports: [AsideComponent, RouterOutlet, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
